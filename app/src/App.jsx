39e86b5bb1ed87becf9fc322d6ab1f35d6327c3c
Abdullah Habib <abdullahhabib.86@gmail.com>
2023-08-16 23:12:10 +0500

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -64,8 +64,10 @@ function App() {
   }
   
    return (
-    <div className='App'>
-      <div style={{ position: "relative", height : "800px", width: "700px"}}>
+    <div className="App">
+     <div style={{ position: "relative", height: "100%", width: "100%", maxWidth: "700px", margin: "0 auto" }}>      <h1 style={{ textAlign: "center", marginBottom: "20px", color: "#A6DAD8", fontSize: "20px" }}>
+          <span style={{ color: "#009591", fontSize: "28px" }}>HBL</span> Demo Chatbot
+        </h1>
         <MainContainer>
           <ChatContainer>
             <MessageList
